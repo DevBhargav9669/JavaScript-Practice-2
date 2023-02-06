53d11df3429d27c52fef0ee97ee5af508a04ffcb
DevBhargav9669 <bhargavvarma96@gmail.com>
2023-02-06 12:55:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* (function square(num) {
+    console.log(num * num);
+}) (7); */
+
+var num1 = 2;
+var num2 = 3;
+
+function multiply() {
+    return num1 * num2;
+};
+
+console.log(multiply());
